2fed5c23e837d51cb70ca5a15a8a8ebd872a1be0
Gabriel <avila.gabi@outlook.com>
2025-04-21 01:21:46 -0300

    Backend funcionando, rutas vehiculos, clientes y reparaciones con conexion a BD, conecciones con frontend react

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_cors import CORS
 from db import db
 from routes.cliente_routes import cliente_bp
+from routes.vehiculo_routes import vehiculo_bp
+from routes.reparacion_routes import reparacion_bp
+
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +16,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 app.register_blueprint(cliente_bp, url_prefix='/api/clientes')
+app.register_blueprint(vehiculo_bp, url_prefix='/api/vehiculos')
+app.register_blueprint(reparacion_bp, url_prefix='/api/reparaciones')
+
 
 if __name__ == '__main__':
     with app.app_context():
